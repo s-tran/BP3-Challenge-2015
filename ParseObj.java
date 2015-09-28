9677a343e7f0adacd9048e265b4bd775b9208339
s-tran <stephen.lance.tran@gmail.com>
2015-09-28 14:22:44 -0500

    Create ParseObj.java

---
+++ ParseObj.java
@@ -0,0 +1,21 @@
+package bp3Challenge;
+
+public class ParseObj {
+	
+	//closeDate, createDate, instanceId, name, assignee, status
+	String closeDate;
+	String createDate;
+	int instanceId;
+	String name;
+	String assignee;
+	String status;
+	
+	public ParseObj() {
+		String closeDate = "0";
+		String createDate = "0";
+		int instanceId = 1;
+		String name = "0";
+		String status = "0";
+	}
+	
+}
